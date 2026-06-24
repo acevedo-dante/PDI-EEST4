1651c10a1497d6eba53fe3511539df21acbc5246
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-06-24 15:49:02 -0300

    Update store.php

--- src/views/productos/store.php
+++ src/views/productos/store.php
@@ -1,1 +1,18 @@
-<h1>Creando Producto</h1>
+<form method="POST" action="/productos">
+    <label>Nombre:</label>
+    <input type="text" name="nombre">
+
+    <br><br>
+
+    <label>Precio:</label>
+    <input type="number" name="precio">
+
+    <br><br>
+
+    <label>Descripción:</label>
+    <input type="text" name="descripcion">
+
+    <br><br>
+
+    <button type="submit">Guardar</button>
+</form>
