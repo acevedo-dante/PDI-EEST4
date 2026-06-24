d6b1b2f7fadc823dc062394696bc0ca3eea25ca2
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-06-24 15:50:55 -0300

    Add product details display in resultado.php

--- src/views/productos/resultado.php
+++ src/views/productos/resultado.php
@@ -1,1 +1,7 @@
+<h1>Producto recibido</h1>
 
+<p><strong>Nombre:</strong> <?= $nombre ?></p>
+
+<p><strong>Precio:</strong> <?= $precio ?></p>
+
+<p><strong>Descripción:</strong> <?= $descripcion ?></p>
