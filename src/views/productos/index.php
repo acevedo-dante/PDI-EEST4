681822375580781389ae40c70ea338e752a1bd75
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-06-24 15:58:12 -0300

    Update index.php

--- src/views/productos/index.php
+++ src/views/productos/index.php
@@ -1,5 +1,21 @@
-<h1>Producto recibido</h1>
+$app->get('/productos', function ($request, $response) use ($renderer) {
 
-<p>Nombre: Laguna</p>
-<p>Precio: "invaluable<3"</p>
-<p>Descripción: Carro</p>
+  $productos = [
+    ['id' => 1, 'name' => 'Camiseta de futbol', 'price' => 15000],
+    ['id' => 2, 'name' => 'Botines', 'price' => 45000],
+    ['id' => 3, 'name' => 'Pelota', 'price' => 2000],
+    ['id' => 4, 'name' => 'Medias deportivas', 'price' => 3000],
+    ['id' => 5, 'name' => 'Guantes de arquero', 'price' => 8000]
+  ];
+
+  $queryParams = $request->getQueryParams();
+  $limit = isset($queryParams['limit']) ? (int)$queryParams['limit'] : null;
+
+  if ($limit) {
+    $productos = array_slice($productos, 0, $limit);
+  }
+
+  return $renderer->render($response, 'productos/index.php', [
+    'productos' => $productos
+  ]);
+});
