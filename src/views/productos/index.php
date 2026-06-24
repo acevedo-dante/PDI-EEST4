e5dd928db9daa9c40799dfb27e72357a9601af8b
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-06-24 16:01:27 -0300

    Update index.php

--- src/views/productos/index.php
+++ src/views/productos/index.php
@@ -1,21 +1,12 @@
-$app->get('/productos', function ($request, $response) use ($renderer) {
+<h1>Listado de Productos</h1>
 
-  $productos = [
-    ['id' => 1, 'name' => 'Camiseta de futbol', 'price' => 15000],
-    ['id' => 2, 'name' => 'Botines', 'price' => 45000],
-    ['id' => 3, 'name' => 'Pelota', 'price' => 2000],
-    ['id' => 4, 'name' => 'Medias deportivas', 'price' => 3000],
-    ['id' => 5, 'name' => 'Guantes de arquero', 'price' => 8000]
-  ];
-
-  $queryParams = $request->getQueryParams();
-  $limit = isset($queryParams['limit']) ? (int)$queryParams['limit'] : null;
-
-  if ($limit) {
-    $productos = array_slice($productos, 0, $limit);
-  }
-
-  return $renderer->render($response, 'productos/index.php', [
-    'productos' => $productos
-  ]);
-});
+<ul>
+    <?php foreach ($productos as $producto): ?>
+        <li>
+            <strong>ID:</strong> <?= $producto['id'] ?> <br>
+            <strong>Nombre:</strong> <?= $producto['name'] ?> <br>
+            <strong>Precio:</strong> $<?= $producto['price'] ?>
+            <hr>
+        </li>
+    <?php endforeach; ?>
+</ul>
