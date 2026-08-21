15b379a5626ef094ebd85c56ce456465c0af8df5
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-08-21 08:43:06 -0300

    Update update.php

--- src/views/productos/update.php
+++ src/views/productos/update.php
@@ -1,1 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Editar Producto</title>
+</head>
+<body>
 
+    <h1>Editar Producto</h1>
+
+    <form method="POST" action="/productos/<?php echo $producto['id']; ?>">
+
+        <input type="hidden" name="_method" value="PUT">
+
+        <label>Nombre:</label>
+        <input
+            type="text"
+            name="nombre"
+            value="<?php echo $producto['nombre']; ?>"
+            required
+        >
+
+        <br><br>
+
+        <label>Descripción:</label>
+        <input
+            type="text"
+            name="descripcion"
+            value="<?php echo $producto['descripcion']; ?>"
+        >
+
+        <br><br>
+
+        <label>Precio:</label>
+        <input
+            type="number"
+            name="precio"
+            value="<?php echo $producto['precio']; ?>"
+        >
+
+        <br><br>
+
+        <label>Stock:</label>
+        <input
+            type="number"
+            name="stock"
+            value="<?php echo $producto['stock']; ?>"
+        >
+
+        <br><br>
+
+        <button type="submit">Actualizar producto</button>
+
+    </form>
+
+    <br>
+
+    <a href="/productos/<?php echo $producto['id']; ?>">Volver</a>
+
+</body>
+</html>
