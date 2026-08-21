9c981cc7a10de0cd52b264eb63f3ebe2b73d02ca
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-08-21 07:19:14 -0300

    Update 001_crear_primera_entidad.sql

--- src/database/migrations/001_crear_primera_entidad.sql
+++ src/database/migrations/001_crear_primera_entidad.sql
@@ -1,5 +1,6 @@
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL
+    nombre VARCHAR(100),
+    email VARCHAR(150),
+    password VARCHAR(255)
 );
