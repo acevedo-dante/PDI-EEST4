5871436dca13ec57f1e794eac5c9e89fca05adbb
Gambeta-latorre <166230769+Gambeta-latorre@users.noreply.github.com>
2026-09-23 18:43:39 -0300

    Refactor user routes to use PhpRenderer

--- src/routes/usuarios.routes.php
+++ src/routes/usuarios.routes.php
@@ -1,11 +1,21 @@
 <?php
 
 use Slim\App;
+use Slim\Views\PhpRenderer;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
-return function (App $app) {
-    // Rutas de usuarios
-    $app->get('/usuarios', function ($request, $response) {
-        // Tu lógica de usuarios aquí
-        return $response;
+return function (App $app, PhpRenderer $renderer) {
+
+    // Listar usuarios
+    $app->get('/usuarios/', function (Request $request, Response $response) use ($renderer) {
+        return $renderer->render($response, 'usuarios/index.php');
     });
+
+    // Mostrar un usuario específico
+    $app->get('/usuarios/{id}', function (Request $request, Response $response, array $args) use ($renderer) {
+        $id = $args['id'];
+        return $renderer->render($response, 'usuarios/show.php', ['id' => $id]);
+    });
+
 };
